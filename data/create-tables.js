5b313244be9c0daefada7a1f339a09803248b83e
Rex Bergeron <rexbergeron@comcast.net>
2020-02-28 09:33:34 -0800

    heroku deploy

---
+++ data/create-tables.js
@@ -0,0 +1,36 @@
+const client = require('./lib/client.js');
+
+run();
+
+async function run() {
+
+    try {
+        // run a query to create tables
+        await client.query(`
+            CREATE TABLE users (
+                id SERIAL PRIMARY KEY,
+                email VARCHAR(256) NOT NULL,
+                hash VARCHAR(512) NOT NULL,
+                display_name VARCHAR(256) NOT NULL
+            );
+        
+            CREATE TABLE favorites (
+                id VARCHAR(64) PRIMARY KEY,
+                pokemon VARCHAR(256) NOT NULL,
+                type_1 VARCHAR(256) NOT NULL,
+                base_experience INTEGER NOT NULL
+            );
+        `);
+
+        console.log('create tables complete');
+    }
+    catch (err) {
+        // problem? let's see the error...
+        console.log(err);
+    }
+    finally {
+        // success or failure, need to close the db connection
+        client.end();
+    }
+    
+}
---
+++ server.js
@@ -0,0 +1,101 @@
+require('dotenv').config();
+
+// Application Dependencies
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const client = require('./data/lib/client');
+// Initiate database connection
+
+
+// Application Setup
+const ensureAuth = require('./data/lib/auth/ensure-auth');
+const createAuthRoutes = require('./data/lib/auth/create-auth-routes');
+const request = require('superagent');
+
+const authRoutes = createAuthRoutes({
+    async selectUser(email) {
+        const result = await client.query(`
+            SELECT id, email, hash, display_name as "displayName" 
+            FROM users
+            WHERE email = $1;
+        `, [email]);
+        return result.rows[0];
+    },
+    async insertUser(user, hash) {
+        console.log(user);
+        const result = await client.query(`
+            INSERT into users (email, hash, display_name)
+            VALUES ($1, $2, $3)
+            RETURNING id, email, display_name;
+        `, [user.email, hash, user.display_name]);
+        return result.rows[0];
+    }
+});
+
+const app = express();
+app.use(morgan('dev')); // http logging
+app.use(cors()); // enable CORS request
+app.use(express.static('public')); // server files from /public folder
+app.use(express.json()); // enable reading incoming json data
+
+app.use(express.json()); // enable reading incoming json data
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/auth', authRoutes);
+
+app.use('/api', ensureAuth);
+
+app.use('/api/me', ensureAuth);
+
+app.get('/api/me/favorites', async (req, res) => {
+    try {
+        const myQuery = `
+            SELECT * FROM favorites
+            WHERE user_id=$1
+        `;
+        
+        const favorites = await client.query(myQuery, [req.userId]);
+        
+        res.json(favorites.rows);
+
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+app.post('/api/me/favorites', async(req, res) => {
+    try {
+        const {
+            pokemon,
+            type_1,
+            base_experience,
+        } = req.body.results;
+
+        const newFavorites = await client.query(`
+            INSERT INTO favorites (pokemon, type_1, base_experience, user_id)
+            values ($1, $2, $3, $4)
+            returning *
+        `, [
+            pokemon, 
+            type_1, 
+            base_experience, 
+            req.userId,
+        ]);
+
+        res.json(newFavorites.rows[0]);
+
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+app.get('/api/pokedex', async (req, res) => {
+    const data = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${req.query.search}`);
+
+    res.json(data.body.results);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('listening at ', process.env.PORT);
+});
